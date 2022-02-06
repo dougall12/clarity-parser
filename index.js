b89ade354867a46b9ee0c46c07f762d19e501462
dougall12 <s.dougall12@hotmail.co.uk>
2022-02-06 06:51:18 +0000

    Added cli command

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+
 import chalk from "chalk";
 import clear from "clear";
 import figlet from "figlet";
 import records from "./modules/parseXML.js";
 import writeCsv from "./modules/writeCsv.js";
-import inquirer from "./inquirer.js";
+import inquirer from "./modules/inquirer.js";
 
 clear();
 
@@ -12,6 +14,7 @@ console.log(chalk.red(figlet.textSync("FP", { horizontalLayout: "full" })));
 const run = async () => {
   //Prompt User for file paths
   const pathObj = await inquirer();
+
   //!Stored as inputPath & outputPath
 
   const XMLpath = pathObj.inputPath;
